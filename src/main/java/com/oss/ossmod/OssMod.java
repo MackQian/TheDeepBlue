b5712bb91ab017c4cfb01bed425d593e1a54ec7c
Trevor Crystal <TGCrystal@users.noreply.github.com>
2021-03-30 23:17:14 -0400

    Revert "break build"
    This reverts commit 54f99d7d9b38fca2fae271c2a34f75d9c19123cb.

--- src/main/java/com/oss/ossmod/OssMod.java
+++ src/main/java/com/oss/ossmod/OssMod.java
@@ -20,7 +20,7 @@ import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("ossmod")
-public class OssModTest
+public class OssMod
 {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();

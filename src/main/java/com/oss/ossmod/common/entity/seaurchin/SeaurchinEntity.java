c6b8f47c22ccde7a02f186c92c86e679ff0b8914
AndrewKhans <khansa@rpi.edu>
2021-04-29 00:03:01 -0400

    Sea Urchin now attacks

--- src/main/java/com/oss/ossmod/common/entity/seaurchin/SeaurchinEntity.java
+++ src/main/java/com/oss/ossmod/common/entity/seaurchin/SeaurchinEntity.java
@@ -1,17 +1,28 @@
 package com.oss.ossmod.common.entity.seaurchin;
 
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.ai.goal.LeapAtTargetGoal;
+import net.minecraft.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
+import net.minecraft.entity.ai.goal.SwimGoal;
+import net.minecraft.entity.monster.SpiderEntity;
 import net.minecraft.entity.passive.WaterMobEntity;
 import net.minecraft.entity.passive.fish.AbstractGroupFishEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.IndirectEntityDamageSource;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.SoundEvents;
+import net.minecraft.world.Difficulty;
 import net.minecraft.world.World;
 
 /*
@@ -25,23 +36,29 @@ public class SeaurchinEntity extends WaterMobEntity {
 
     }
 
-//    protected SoundEvent getAmbientSound() {
-//        return SoundEvents.SLIME_SQUISH_SMALL;
-//    }
+    protected void registerGoals() {
+        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, PlayerEntity.class, true));
+        this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 1.0D, false));
+    }
 
-//    protected SoundEvent getDeathSound() {
-//        return SoundEvents.SLIME_DEATH;
-//    }
-
-//    protected SoundEvent getHurtSound(DamageSource p_184601_1_) {
-//        return SoundEvents.SLIME_HURT_SMALL;
-//    }
+    public boolean doHurtTarget(Entity p_70652_1_) {
+        if (super.doHurtTarget(p_70652_1_)) {
+            if (p_70652_1_ instanceof LivingEntity) {
+                int i = 3;
+                ((LivingEntity)p_70652_1_).addEffect(new EffectInstance(Effects.POISON, i * 20, 0));
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     public static AttributeModifierMap.MutableAttribute prepareAttributes() {
         return LivingEntity.createLivingAttributes()
-                .add(Attributes.ATTACK_DAMAGE, 3.0D)
+                .add(Attributes.ATTACK_DAMAGE, 0.5D)
                 .add(Attributes.MAX_HEALTH, 5.0D)
-                .add(Attributes.FOLLOW_RANGE, 40.0D);
+                .add(Attributes.FOLLOW_RANGE, Double.MIN_NORMAL)
+                .add(Attributes.MOVEMENT_SPEED, Double.MIN_NORMAL)
+                .add(Attributes.ATTACK_KNOCKBACK, 0.5D);
     }
-
 }
